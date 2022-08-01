7237f2c390168b54b0322ca05c5ca11d8f80b89a
Daniel Chukwu <00danzy@gmail.com>
2022-08-01 17:00:25 +0100

    comments

--- src/Exeat.java
+++ src/Exeat.java
@@ -15,14 +15,21 @@ public class Exeat {
         f.setSize(600, 600);
         f.setLayout(new GridLayout(7, 1));
 
+        // Create labels and text field
+
+        // reason: label and textfield
         JLabel reasonLabel = new JLabel("Reason: ");
         JTextField reasonField = new JTextField();
+
+        // name: label and textfield
         JLabel nameLabel = new JLabel("Name: ");
         JTextField nameField = new JTextField();
+
+        // matric: label and textfield
         JLabel matricLabel = new JLabel("Matric Number: ");
         JTextField matricNumberField = new JTextField();
 
-        // add labels and textfields
+        // add labels and text-fields
         f.add(reasonLabel);
         f.add(reasonField);
 
@@ -32,24 +39,26 @@ public class Exeat {
         f.add(matricLabel);
         f.add(matricNumberField);
 
-        // create btn
+        // create take exeat button
         JButton btn = new JButton("Take Exeat");
         f.add(btn);
 
+        // add listener on the button
         btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 f.setVisible(false);
+                // Get the values inputed into the fields
                 String reasonText = reasonField.getText();
                 String nameText = nameField.getText();
                 String matricText = matricNumberField.getText();
 
+                // Write recieved input into a file
                 try {
-                    FileWriter writer = new FileWriter("exeats taken.txt");
+                    FileWriter writer = new FileWriter("exeats.txt");
                     writer.append(reasonText + "\n");
                     writer.append(nameText + "\n");
                     writer.append(matricText + "\n\n");           // \n means add new line
-
 
                     writer.close();
                 } catch (Exception x) {
